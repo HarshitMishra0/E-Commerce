690c6392bb73e916d4607dc063492b3739720cbd
Harshit Mishra <harshitmishra.inbox@gmail.com>
2024-02-24 23:39:48 +0530

    updated the arrow bug

--- frontend/src/customer/Pages/HomePage/HomePage.jsx
+++ frontend/src/customer/Pages/HomePage/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import MainCrosel from "../../components/HomeCarosel/MainCarosel";
 import HomeSectionCarosel from "../../components/HomeSectionCarosel/HomeSectionCarosel";
+import { kurta } from "../../../Data/Men's Kurta/kurta";
 
 const HomePage = () => {
   return (
@@ -8,11 +9,11 @@ const HomePage = () => {
       <MainCrosel />
 
       <div className="space-y-10 py-20 flex flex-col justify-center px-5 lg:px-10">
-        <HomeSectionCarosel />
-        <HomeSectionCarosel />
-        <HomeSectionCarosel />
-        <HomeSectionCarosel />
-        <HomeSectionCarosel />
+        <HomeSectionCarosel data={kurta} sectionName={"Men's Kurta"} />
+        <HomeSectionCarosel data={kurta} sectionName={"Men's Shoes"} />
+        <HomeSectionCarosel data={kurta} sectionName={"Men's Shirt"} />
+        <HomeSectionCarosel data={kurta} sectionName={"Women's Saree"} />
+        <HomeSectionCarosel data={kurta} sectionName={"Women's Dress"} />
       </div>
     </div>
   );
--- frontend/src/customer/components/HomeSectionCarosel/HomeSectionCarosel.jsx
+++ frontend/src/customer/components/HomeSectionCarosel/HomeSectionCarosel.jsx
@@ -1,12 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import AliceCarousel from "react-alice-carousel";
 import HomeSectionCard from "../HomeSectionCard/HomeSectionCard";
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 import { Button } from "@mui/material";
-import { kurta } from "../../../Data/Men's Kurta/kurta";
+import "@mui/material/styles";
 
-const HomeSectionCarosel = () => {
+const HomeSectionCarousel = ({ data, sectionName }) => {
   const [activeIndex, setActiveIndex] = useState(0);
+  const carouselRef = useRef(null);
+
   const responsive = {
     0: { items: 1 },
     720: { items: 3 },
@@ -16,23 +18,34 @@ const HomeSectionCarosel = () => {
   const slidePrev = () => {
     if (activeIndex > 0) {
       setActiveIndex(activeIndex - 1);
+      carouselRef.current.slidePrev();
     }
   };
-  const slideNext = () => setActiveIndex(activeIndex + 1);
-  const syncActiveIndex = ({ item }) => setActiveIndex(item);
 
-  const items = kurta
+  const slideNext = () => {
+    if (activeIndex < data.length - 1) {
+      setActiveIndex(activeIndex + 1);
+      carouselRef.current.slideNext();
+    }
+  };
+
+  const items = data
     .slice(0, 10)
-    .map((item) => <HomeSectionCard product={item} />);
+    .map((item, index) => <HomeSectionCard key={index} product={item} />);
+
   return (
     <div className="border">
+      <h2 className="text-2x1 font-extrabold text-gray-800 py-5">
+        {sectionName}
+      </h2>
       <div className="relative p-5">
         <AliceCarousel
           items={items}
           responsive={responsive}
           disableDotsControls
-          onSlideChanged={syncActiveIndex}
+          disableButtonsControls
           activeIndex={activeIndex}
+          ref={(el) => (carouselRef.current = el)}
         />
         {activeIndex !== items.length - 5 && (
           <Button
@@ -47,8 +60,7 @@ const HomeSectionCarosel = () => {
             }}
             aria-label="next"
           >
-            {" "}
-            <ArrowBackIosNewIcon sx={{ transform: "rotate(90deg)" }} />{" "}
+            <ArrowBackIosNewIcon sx={{ transform: "rotate(90deg)" }} />
           </Button>
         )}
 
@@ -65,7 +77,7 @@ const HomeSectionCarosel = () => {
             }}
             aria-label="prev"
           >
-            <ArrowBackIosNewIcon sx={{ transform: "rotate(90deg)" }} />{" "}
+            <ArrowBackIosNewIcon sx={{ transform: "rotate(90deg)" }} />
           </Button>
         )}
       </div>
@@ -73,4 +85,4 @@ const HomeSectionCarosel = () => {
   );
 };
 
-export default HomeSectionCarosel;
+export default HomeSectionCarousel;
