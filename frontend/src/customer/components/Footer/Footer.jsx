634626de84867812ce59a74931e4ee6af6b34f5f
Harshit Mishra <harshitmishra.inbox@gmail.com>
2024-02-27 11:43:45 +0530

    update

--- frontend/src/customer/components/Footer/Footer.jsx
+++ frontend/src/customer/components/Footer/Footer.jsx
@@ -1,4 +1,4 @@
-import { Grid, Typography } from "@mui/material";
+import { Button, Grid, Typography } from "@mui/material";
 import React from "react";
 
 const Footer = () => {
@@ -13,12 +13,105 @@ const Footer = () => {
           <Typography className="pb-5" variant="h6">
             Company
           </Typography>
+          <div>
+            <Button className="pb-5" variant="h6" gutterBottom>
+              About
+            </Button>
+          </div>
+
+          <div>
+            {" "}
+            <Button className="pb-5" variant="h6" gutterBottom>
+              Blog
+            </Button>
+          </div>
+
+          <div>
+            <Button className="pb-5" variant="h6" gutterBottom>
+              Press
+            </Button>
+          </div>
+          <div>
+            <Button className="pb-5" variant="h6" gutterBottom>
+              Jobs
+            </Button>
+          </div>
+          <div>
+            <Button className="pb-5" variant="h6" gutterBottom>
+              Partners
+            </Button>
+          </div>
+        </Grid>
+        <Grid item xs={12} md={3}>
           <Typography className="pb-5" variant="h6">
-            About
+            Solutions
           </Typography>
+          <div>
+            <Button className="pb-5" variant="h6" gutterBottom>
+              Marketing
+            </Button>
+          </div>
+
+          <div>
+            <Button className="pb-5" variant="h6" gutterBottom>
+              Analytics
+            </Button>
+          </div>
+
+          <div>
+            <Button className="pb-5" variant="h6" gutterBottom>
+              Commerce
+            </Button>
+          </div>
+          <div>
+            <Button className="pb-5" variant="h6" gutterBottom>
+              Insights
+            </Button>
+          </div>
+          <div>
+            <Button className="pb-5" variant="h6" gutterBottom>
+              Support
+            </Button>
+          </div>
+        </Grid>
+        <Grid item xs={12} md={3}>
           <Typography className="pb-5" variant="h6">
-            Company
+            Documentation
           </Typography>
+          <div>
+            <Button className="pb-5" variant="h6" gutterBottom>
+              Guides
+            </Button>
+          </div>
+
+          <div>
+            <Button className="pb-5" variant="h6" gutterBottom>
+              API Status
+            </Button>
+          </div>
+        </Grid>
+        <Grid item xs={12} md={3}>
+          <Typography className="pb-5" variant="h6">
+            Legal
+          </Typography>
+          <div>
+            <Button className="pb-5" variant="h6" gutterBottom>
+              Claim
+            </Button>
+          </div>
+
+          <div>
+            {" "}
+            <Button className="pb-5" variant="h6" gutterBottom>
+              Privacy
+            </Button>
+          </div>
+
+          <div>
+            <Button className="pb-5" variant="h6" gutterBottom>
+              Terms
+            </Button>
+          </div>
         </Grid>
       </Grid>
     </div>
